28e50e08ac2e691df51b035f120669434238dbd6
Shadowmataj <christianmataj1@gmail.com>
2024-04-10 08:34:19 -0600

    Se corrigen nombres de variables

--- Clase_6_entregable/src/app.js
+++ Clase_6_entregable/src/app.js
@@ -8,14 +8,14 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get("/products", async (req, res) => {
     let limitproducts = req.query.limit
-    const productList = await JSON.parse(fs.readFileSync("./src/productList.json"))
+    const productsList = await JSON.parse(fs.readFileSync("./src/products_list.json"))
     const filteredList = []
 
-    if (!limitproducts) return res.send({ productList })
+    if (!limitproducts) return res.send({ productsList })
 
     for (let i = 0; i < limitproducts; i++) {
-        if (productList[i]) {
-            filteredList.push(productList[i])
+        if (productsList[i]) {
+            filteredList.push(productsList[i])
         }
     }
     res.send({ filteredList })
@@ -24,7 +24,7 @@ app.get("/products", async (req, res) => {
 
 app.get("/products/:pid", async (req, res) => {
 
-    const productList = await JSON.parse(fs.readFileSync("./src/productList.json"))
+    const productList = await JSON.parse(fs.readFileSync("./src/products_list.json"))
     const product = productList.find(item => item.id == req.params.pid)
     if (!product) return res.send({ Error: `ID no encontrado: ${req.params.pid}` })
     res.send({ product })
