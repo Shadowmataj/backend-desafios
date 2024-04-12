febf9f60ea2eca2fef8540a830704d4a5bf6e2a6
Shadowmataj <christianmataj1@gmail.com>
2024-04-12 08:34:57 -0600

    importing ProductManager from ProductManagers.js to make code on app.js more easy to understand

--- Clase_6_entregable/src/ProductManagers.js
+++ Clase_6_entregable/src/ProductManagers.js
@@ -1,16 +1,18 @@
-import fs from "fs"
-
-if(!fs.existsSync("./src/products_list.json")){
+import fs, { read } from "fs"
+// search for file "./src/products_list.json", it creates it if doesn't exist
+if (!fs.existsSync("./src/products_list.json")) {
     fs.writeFileSync("./src/products_list.json", JSON.stringify([]))
 }
-
+// create a class ProductManager to manage all the products we need.
 export class ProductManager {
+    // the constructor creates all the elements we need in our product manager 
     constructor() {
-        this.path = "./src/products_list.json"
-        this.products = JSON.parse(fs.readFileSync(this.path))
-        this.id = Math.max(...this.products.map(item => item.id))
+        this.path = "./src/products_list.json" //defines the path file to use
+        this.products = JSON.parse(fs.readFileSync(this.path)) //read the file and asign the data to this.products
+        this.id = Math.max(...this.products.map(item => item.id))//identifies the max id number in our data base and asign it to avoid duplication
     }
 
+    // async function to add products into de data base
     async addProduct(title, description, price, thumbnail, code, stock) {
 
         const codeVerification = this.products.some(item => item.code == code)
@@ -30,65 +32,63 @@ export class ProductManager {
             console.log(`ERROR: El code "${code}" ya ha sido utilizado en otro producto`)
         }
     }
-    getProducts() {
+    //function to get a certain amount of products or the entire array
+    getProducts(limit) {
         this.products = JSON.parse(fs.readFileSync(this.path))
-        return this.products
+        return limit === 0 ? this.products : this.products.splice(0,limit)
     }
-
-    getProductbyId(code) {
+    //funtion to get a specific product by id
+    getProductbyId(pid) {
         this.products = JSON.parse(fs.readFileSync(this.path))
-        const item = this.products.find(resp => resp.code == code)
-        if (item) {
-            console.log(item)
-        } else {
-            console.log(`ERROR: Not found: "${code}"`)
-        }
+        const item = this.products.find(resp => resp.id == pid)
+        if (!item) return {ERROR: `ID Not found: ${pid}`}
+        return item 
     }
+    //async function to erase a product in our database
+    async deleteProduct(id) {
 
-    async deleteProduct(id){
-
-        const condicion = this.products.some((item => item.id == id))
-        if (condicion){
+        const product = this.products.some((item => item.id == id))
+        if (product) {
             this.products = this.products.filter(resp => resp.id !== id)
             await fs.promises.writeFile(this.path, JSON.stringify(this.products))
         } else {
             console.log(`ERROR: ID Not found: "${id}"`)
         }
-    
+
     }
 
-    async updateProduct(id, property = "", change = ""){
+    async updateProduct(id, property = "", change = "") {
         const condicion = this.products.some((item => item.id === id))
-        
+
 
         const idList = this.products.map(item => item.id)
         const index = idList.indexOf(id)
 
-        if (condicion){
+        if (condicion) {
 
-            let {title, description, price, thumbnail, code, stock} = this.products.filter(resp => resp.id === id)[0]
+            let { title, description, price, thumbnail, code, stock } = this.products.filter(resp => resp.id === id)[0]
 
-            if (property === "title"){
+            if (property === "title") {
                 title = change
             }
 
-            if (property === "description"){
+            if (property === "description") {
                 description = change
             }
 
-            if (property === "price"){
+            if (property === "price") {
                 price = change
             }
 
-            if (property === "thumbnail"){
+            if (property === "thumbnail") {
                 thumbnail = change
             }
-            
-            if (property === "code"){
+
+            if (property === "code") {
                 code = change
             }
 
-            if (property === "stock"){
+            if (property === "stock") {
                 stock = change
             }
 
@@ -101,20 +101,17 @@ export class ProductManager {
                 code: code,
                 stock: stock
             }
-
             this.products.splice(index, 1, item)
-
             await fs.promises.writeFile(this.path, JSON.stringify(this.products))
-
         } else {
             console.log(`ERROR: ID Not found: "${id}"`)
         }
     }
-    
+
 }
 
 
-// const productManager1 = new ProductManager() // Se genera el product manager 
+// const productManager1 = new ProductManager() // Se genera el product manager
 
 // console.log(productManager1.getProducts()) 
 
--- Clase_6_entregable/src/app.js
+++ Clase_6_entregable/src/app.js
@@ -9,26 +9,15 @@ app.use(express.urlencoded({ extended: true }))
 
 // Endpoint con parámetro y query para obtener array completo o limitar la cantidad de productos
 app.get("/products", async (req, res) => {
-    let limitproducts = req.query.limit
-    const productsList = await pm.getProducts()
-    const filteredList = []
-
-    if (!limitproducts) return res.send({ payload: productsList })
-
-    for (let i = 0; i < limitproducts; i++) {
-        if (productsList[i]) {
-            filteredList.push(productsList[i])
-        }
-    }
-    res.send({ payload: filteredList })
+    let limitproducts = req.query.limit || 0
+    const productsList = await pm.getProducts(limitproducts)
+    res.send({ payload: productsList })
 })
 
 // endpoint con parámetro para obtener produto por id
 app.get("/products/:pid", async (req, res) => {
 
-    const productList = await pm.getProducts()
-    const product = productList.find(item => item.id == req.params.pid)
-    if (!product) return res.send({ Error: `ID no encontrado: ${req.params.pid}` })
+    const product = await pm.getProductbyId(req.params.pid)
     res.send({ payload: product })
 })
 
