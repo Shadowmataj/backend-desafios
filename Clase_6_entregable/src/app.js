42043dc115fa5617949303c84a1b8546bff712d1
Shadowmataj <christianmataj1@gmail.com>
2024-04-12 08:39:50 -0600

    english please

--- Clase_6_entregable/src/app.js
+++ Clase_6_entregable/src/app.js
@@ -6,16 +6,15 @@ const pm = new ProductManager()
 
 app.use(express.urlencoded({ extended: true }))
 
-// Endpoint con parámetro y query para obtener array completo o limitar la cantidad de productos
+// Endpoint using query to get a complete array or just a limited one
 app.get("/products", async (req, res) => {
     let limitproducts = req.query.limit || 0
     const productsList = await pm.getProducts(limitproducts)
     res.send({ payload: productsList })
 })
 
-// endpoint con parámetro para obtener produto por id
+// Endpoint with paramto get a product by id
 app.get("/products/:pid", async (req, res) => {
-
     const product = await pm.getProductbyId(req.params.pid)
     res.send({ payload: product })
 })
